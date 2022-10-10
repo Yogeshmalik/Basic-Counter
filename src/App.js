bf6254ac01dfff0fef96548184b61a6f0f921f65
Yogesh Malik <ysmworking@gmail.com>
2022-10-10 22:28:06 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react'
 function App() {
+  const [count, setCount] = useState(0)
+  function zero() {
+    setCount(0)
+  }  
+  function increment() {
+    setCount(prevCount => prevCount + 1)
+  }
+  function decrement() {
+    setCount(prevCount => prevCount - 1)
+  }
+  function square() {
+    setCount(prevCount => prevCount * prevCount)
+  }
+  function squareRoot() {
+    setCount(prevCount => Math.sqrt(prevCount))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className='hero'>COUNTER!</h2>
+      <h1>{count}</h1>
+      <button onClick={zero}>Reset</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={square}>Square</button>
+      <button onClick={squareRoot}>SquareRoot</button>
+      <footer>
+        <p>Basic counter using reactJS (Y)</p>
+      </footer>
     </div>
   );
 }
